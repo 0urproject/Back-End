d3477a5befb721ea4e156ecb2bd1ff4d2aad76dc
Ze Nildo Pereira <xnildo.dev@gmail.com>
2021-07-02 18:49:04 -0300

    02/07/2021- Classe Postagem criada

--- aMEI/src/main/java/br/generation/grupo4/aMEI/AMeiApplication.java
+++ aMEI/src/main/java/br/generation/grupo4/aMEI/AMeiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AMeiApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AMeiApplication.class, args);
 	}
---
+++ aMEI/src/main/java/br/generation/grupo4/aMEI/model/Postagem.java
@@ -0,0 +1,5 @@
+package br.generation.grupo4.aMEI.model;
+
+public class Postagem {
+
+}
