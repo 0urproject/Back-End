99b335c73f005af7ac4ecccf447c75cb97f85080
joaovitormo <vitormendes388@gmail.com>
2021-08-23 10:58:02 -0300

    23/08/2021 - Ultimas alterações na model usuario

---
+++ aMEI/src/main/java/br/generation/grupo4/aMEI/model/Tema.java
@@ -0,0 +1,91 @@
+package br.generation.grupo4.aMEI.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id // ID
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO-INCREMENTO
+	private long id;
+	
+	@NotEmpty
+	private String nome;
+	
+	private String keyword;
+	
+	@NotEmpty
+	private String descricao;
+	
+	@Transient
+	private int qtdTema;
+	
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagem;
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	public int getQtdTema() {
+		return qtdTema;
+	}
+
+	public void setQtdTema(int qtdTema) {
+		this.qtdTema = qtdTema;
+	}
+	
+	
+}
---
+++ aMEI/src/main/java/br/generation/grupo4/aMEI/model/UsuarioLogin.java
@@ -0,0 +1,101 @@
+package br.generation.grupo4.aMEI.model;
+
+import java.time.LocalDate;
+
+public class UsuarioLogin {
+	
+	private long id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	private String token;
+	private LocalDate dataNascimento; 
+	private String foto;
+	private String descricaoPerfil;
+	private String razaoSocial;
+	private String linkedin;
+	private String github;
+	private String numero;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getDescricaoPerfil() {
+		return descricaoPerfil;
+	}
+	public void setDescricaoPerfil(String descricaoPerfil) {
+		this.descricaoPerfil = descricaoPerfil;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getLinkedin() {
+		return linkedin;
+	}
+	public void setLinkedin(String linkedin) {
+		this.linkedin = linkedin;
+	}
+	public String getGithub() {
+		return github;
+	}
+	public void setGithub(String github) {
+		this.github = github;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+}
